074eb1e8e63d0d82583f7827b3d16141e7e4f281
DCartist <pbannerman13@gmail.com>
2019-12-23 22:53:21 -0500

    adjust css and information on index.html

--- src/Components/NameButtons/NameButton.js
+++ src/Components/NameButtons/NameButton.js
@@ -12,10 +12,10 @@ class NameButton extends Component {
         }
     }
     render() {
-        const divStyleOutside = {
-            // display: 'inline-block',
-            backgroundColor: 'black'
-              }
+        // const divStyleOutside = {
+        //     // display: 'inline-block',
+        //     backgroundColor: 'black'
+        //       }
         return (
             <div key={this.props.language}>
                  {/* <Link to={"/language/" + this.props.language} params={{ language: this.props.language, info: this.props.info, name:this.props.name }}>
@@ -23,9 +23,13 @@ class NameButton extends Component {
                 {this.props.name}
             </li></Link> */}
                  <Link to={"/language/" + this.props.language} params={{ language: this.props.language, info: this.props.info, name:this.props.name }}>
-                <li key={this.props.language} style={divStyleOutside}>
+                <li key={this.props.language}>
                 {this.props.name}
             </li></Link>
+                 {/* <Link to={"/language/" + this.props.language} params={{ language: this.props.language, info: this.props.info, name:this.props.name }}>
+                <li key={this.props.language} style={divStyleOutside}>
+                {this.props.name}
+            </li></Link> */}
             </div>
         );
     }
