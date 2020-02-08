734d297facdba377cb8601de79f546491a980f98
DCartist <pbannerman13@gmail.com>
2020-02-08 13:42:29 -0500

    adding to proposal js

--- src/Components/Proposal/Proposal.js
+++ src/Components/Proposal/Proposal.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './Proposal.css'
 
 class Proposal extends Component {
     render() {
         return (
             <div>
-                Proposal piece 1
+              
+			<div className="mainBox">
+	<p>Language, by definition, is the method of human communication, either spoken or written, consisting of the use of words in a structured and conventional way. There is a beauty in the fact that humans have more than 600 forms of communicating with machines. </p>
+	
+	<p>
+		The Elements Of is a multidimensional project that shows the human side and the artistic side of technology. The part of the project takes the programming languages and puts them in the format of the periodical table. Each element will have the information about the language interviews with the person or people who made it as well or interviews with historians about the language. This project will be turned into an app as well as made into a book.
+
+	</p>
+	</div>
                 
             </div>
         );
