2507bb1ca120f06f4b232ddeeb90435838ae6b79
dcartist <pbannerman13@gmail.com>
2020-05-29 12:19:44 -0400

    adding contact

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './NavBar.css'
 // import Name from '../Names/Names'
 // import About from '../About/About.js'
+import ContactImage from '../../Images/email.png'
 import Logo from '../../Images/elemof.png'
 import { Link} from "../../../node_modules/react-router-dom";
 class NavBar extends Component {
@@ -13,6 +14,8 @@ class NavBar extends Component {
                
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><a href="mailto:Pbannerman13@gmail.com">Contact</a></li>
+                    <li><a href="mailto:Pbannerman13@gmail.com"><img src={ContactImage}/></a></li>
                 </ul>
             </div>
         );
