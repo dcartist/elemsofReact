d1e54031cc8c7e2a39723a16a5489cee66f26290
DCartist <pbannerman13@gmail.com>
2020-11-08 22:05:25 -0500

    updating index

--- src/Components/2020/Index.js
+++ src/Components/2020/Index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import "./style.css"
+import axios from "axios"
+
 class Index extends Component {
+    constructor(){
+      this.state={
+          nameSearched:"",
+          programResults:[]
+      }  
+    }
+    programSearch = (e)=>{
+        e.preventDefault()
+        this.setState({nameSearched: e.target.value})
+
+
+    }
     render() {
         return (
             <div>
-                
+                <inpu value="" type="text" onChange={this.programSearch}></inpu>
             </div>
         );
     }
