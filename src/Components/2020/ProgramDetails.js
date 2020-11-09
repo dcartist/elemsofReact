d222b5b26a1af6ebe5924ad6bd3c1d50dedb359f
DCartist <pbannerman13@gmail.com>
2020-11-08 21:52:39 -0500

    Changing entire APP for 2020

---
+++ src/Components/2020/ProgramDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProgramDetails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProgramDetails;
