ba84b176880d04d5dd4d1ac5749225c530675e60
DCartist <pbannerman13@gmail.com>
2020-11-10 15:38:44 -0500

    updating modal

--- src/Components/2020/Modal.js
+++ src/Components/2020/Modal.js
@@ -1,40 +1,33 @@
 import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Modal } from 'semantic-ui-react'
 import parse from 'html-react-parser';
 
 function ModalDisplay(props) {
   const [open, setOpen] = React.useState(false)
 
-  return (
-    <Modal
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open}
-  trigger={<Button>{props.name}</Button>}
-    >
-      <Modal.Header>Select a Photo</Modal.Header>
-      <Modal.Content>
-        <Modal.Description>
-          <Header>{props.name}</Header>
-          <p>
-          {parse(props.summary_html) }
-          </p>
-        </Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='black' onClick={() => setOpen(false)}>
-          Nope
-        </Button>
-        <Button
-          content="Yep, that's me"
-          labelPosition='right'
-          icon='checkmark'
-          onClick={() => setOpen(false)}
-          positive
-        />
-      </Modal.Actions>
-    </Modal>
-  )
+  return (<Modal
+    onClose={() => setOpen(false)}
+    onOpen={() => setOpen(true)}
+    open={open}
+trigger={<Button>{props.name}</Button>}
+  >
+    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <Header>{props.name}</Header>
+        {parse(String(props.summary_html))}
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button
+        content="Super Cool"
+        labelPosition='right'
+        icon='checkmark'
+        onClick={() => setOpen(false)}
+        color = "black"
+      />
+    </Modal.Actions>
+  </Modal>)
 }
 
 export default ModalDisplay
