9b1fa89153b953ada4c4903ed4950427ad35af5b
DCartist <pbannerman13@gmail.com>
2020-11-11 13:55:21 -0500

    Updating button

--- src/Components/2020/Button.js
+++ src/Components/2020/Button.js
@@ -6,9 +6,9 @@ import './style.css'
 const ButtonSelection = (props) => (
   <div>
       <Button.Group className="buttonGroupSpace">
-    <Button attached='left' onClick={() => props.apiSelection("name")} inverted color='red' className="buttonSpace"> Search By Name </Button>
+    <Button attached='left' onClick={() => props.apiSelection("name")} inverted color='black' className="buttonSpace"> Search By Name </Button>
     <Button.Or className="buttonSpaceOnOr"/>
-    <Button attached='right' onClick={() => props.apiSelection("id")} inverted color='red' className="buttonSpace"> Search by ID </Button>
+    <Button attached='right' onClick={() => props.apiSelection("id")} inverted color='black' className="buttonSpace"> Search by ID </Button>
     </Button.Group>
   </div>
 )
