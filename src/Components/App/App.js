2a6072be1f82d892a8d6c1d9cfc65e178c7de187
DCartist <pbannerman13@gmail.com>
2020-11-11 14:18:36 -0500

    update app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-// import logo from './logo.svg';
 import './App.css';
 import New2020 from "../2020/Index"
 import New2020Navigation from "../2020/Navigation"
-// import Name from '../Names/Names'
 import About from '../2020/About.js'
-// import Language from '../Language/Language'
-// import Trial from '../Trial/Trial'
 import {Route, Switch} from "react-router-dom";
-// import 'primereact/resources/themes/nova-light/theme.css';
-// import 'primereact/resources/primereact.min.css';
-// import 'primeicons/primeicons.css';
-// import {Route, Link, Switch, Redirect} from "react-router-dom";
 import Home from '../2020/Main'
 
 class App extends Component {
@@ -38,9 +30,7 @@ constructor(){
         <Route path="/languages" exact component={New2020}/>
         <Route path="/2020" exact component={New2020}/>
         <Route path="/2020/Nav" exact component={New2020Navigation}/>
-        {/* <Route path="/trial" component={Trial}/>
-          <Route path="/language/:language" render={(props)=> <Language setLanguage={this.setLanguage} {...props} {...this.state} />} /> */}
-          </Switch>
+        </Switch>
       </div>
     );
   }
